7bfc27766bd312492f4be5c4bea6271b13650996
apstld-prog <apstld@gmail.com>
2025-08-28 15:22:15 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 # ==== CONFIG ====
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8290365345:AAE1bjU7nJtvWzc4uyovji5zNm8Tjz_WJ0g
+BOT_TOKEN = os.getenv("8290365345:AAE1bjU7nJtvWzc4uyovji5zNm8Tjz_WJ0g
 ")
 COINGECKO_API = "https://api.coingecko.com/api/v3/simple/price"
 CHECK_INTERVAL_SEC = int(os.getenv("CHECK_INTERVAL_SEC", "60"))
