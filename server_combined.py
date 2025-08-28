1a31a46114efeb5e069acb76193a3bcdb53f28f7
apstld-prog <apstld@gmail.com>
2025-08-28 20:28:15 +0300

    Add files via upload

--- server_combined.py
+++ server_combined.py
@@ -2,7 +2,7 @@
 import os, threading, time
 from payments_webhook import app as flask_app
 from bot import run_bot
-from waitress import serve  # production WSGI server
+from waitress import serve
 
 def run_flask():
     port = int(os.getenv("PORT", "8000"))
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     t = threading.Thread(target=run_flask, daemon=True)
     t.start()
     time.sleep(1)
-    run_bot()
+    run_bot()  # 
