1b83a41f1e6042f7fff7d6aaed0d3ac50fb2da50
apstld-prog <apstld@gmail.com>
2025-08-29 11:25:21 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 # ==== CONFIG ====
-BOT_TOKEN = os.getenv(""BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 COINGECKO_API = "https://api.coingecko.com/api/v3/simple/price"
 CHECK_INTERVAL_SEC = int(os.getenv("CHECK_INTERVAL_SEC", "60"))
 PAYPAL_SUBSCRIBE_PAGE = os.getenv("PAYPAL_SUBSCRIBE_PAGE", "https://crypto-alerts-bot-k8i7.onrender.com/subscribe.html")
